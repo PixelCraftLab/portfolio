66c2d6a1214ca948017fb62e15b146507b24cecf
PixelCraftLab <vishalgowdask65@gmail.com>
2026-02-26 21:56:02 +0530

    added motion to project

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -28,9 +28,20 @@ const Projects = () => {
             className="absolute inset-0 w-full h-full object-cover z-10 animate-pulse"
         /> */}
 
-        <h2 className="text-heading">My Projects</h2>
+        <motion.h2 className="text-heading "
+            initial={{ opacity: 0, x: -100 }}
+            whileInView={{ opacity: 1, x: 0 }}
+            transition={{ duration: 0.9, ease: "easeOut" }}
+            viewport={{ once: true, amount: 0.5 }}
+            
+        >
+            My Projects
+        </motion.h2>
+
         <div className="bg-gradient-to-r from-transparent via-neutral-700 to-transparent mt-12 h-[1px] w-full"/>
+
         {myProjects.map((project)=>(<Project key={project.id}{...project} setPreview={setPreview}/>))}
+        
         {preview &&(<motion.img className="fixed top-0 left-0z-50 object-cover h-56 rounded-lg pointer-events-none w-80"
         style={{x:springX, y:springY}}
         src={preview}
