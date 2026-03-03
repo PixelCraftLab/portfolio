19a7b27491f65624515b4f90f845267728540bc4
PixelCraftLab <vishalgowdask65@gmail.com>
2026-03-04 03:23:04 +0530

    removed bacground image of graph

--- src/sections/Graphs.jsx
+++ src/sections/Graphs.jsx
@@ -7,12 +7,11 @@ const Graphs = () => {
   return (
     <div  className='relative h-full w-full overflow-hidden mt-10 '
           style={{
-            height: '700px',
+          
            
-            backgroundImage: `radial-gradient(circle, rgba(0,0,0,0) 30%, rgba(0,0,0,1) 75%), url('/assets/projects/HackerImg.png')`,
-            backgroundRepeat: 'no-repeat',
-            backgroundSize: '50% ', 
-            backgroundPosition: 'center',
+            backgroundImage: "url(/assets/projects/sky.jpg)",
+            
+            backgroundSize:"cover",
             
             
             }}
