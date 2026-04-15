3f201cf964b6e6ebcb111dc9ee217afb051413ae
PixelCraftLab <vishalgowdask65@gmail.com>
2026-04-15 13:50:49 +0530

    liberary added for custom cursor

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,49 @@
 import React from 'react'
 import Navbar from './sections/Navbar.jsx'
 import Hero from "./sections/Hero.jsx"
-import  About  from './sections/About.jsx';
+import About from './sections/About.jsx';
 import Projects from './sections/Projects.jsx';
 import Graphs from './sections/Graphs.jsx';
-import CustomCursor from './sections/CustomCursor.jsx';
+// import CustomCursor from './sections/CustomCursor.jsx';
+import Ribbons from "./components/Ribbons.jsx";
 
 
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl ">
-    <Navbar />  
-    <section id="home"><Hero /></section>
-    <section id="about"><About /></section>
-    <section id="graphs"><Graphs /></section>
-    <section id="projects"><Projects /></section>
-    <CustomCursor/>
-    
-    {/* <Hero />
+      <div className="fixed inset-0 z-50">
+        <Ribbons
+          baseThickness={19}
+          colors={["#5227FF", "#06a597"]}
+          speedMultiplier={0.55}
+          maxAge={700}
+          enableFade
+          enableShaderEffect
+        />
+      </div>
+      <Navbar />
+      <section id="home"><Hero /></section>
+      <section id="about"><About /></section>
+      <section id="graphs"><Graphs /></section>
+      <section id="projects"><Projects /></section>
+
+      {/* <CustomCursor/> */}
+
+      {/* <Hero />
     <About/>
     <Graphs/>
     <Projects/>
     <CustomCursor/> */}
-    
-    
-    
-    
-    {/* experience */}
-    {/* contact */}
-    {/* footer */}
+
+
+
+
+      {/* experience */}
+      {/* contact */}
+      {/* footer */}
     </div>
 
-    
+
   );
 };
 
