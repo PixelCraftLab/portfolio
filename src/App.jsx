2724779975289fed7c01a18e65abb0d11211fb06
PixelCraftLab <vishalgowdask65@gmail.com>
2026-04-18 01:33:37 +0530

    changed custom cursor

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,13 @@ import Hero from "./sections/Hero.jsx"
 import About from './sections/About.jsx';
 import Projects from './sections/Projects.jsx';
 import Graphs from './sections/Graphs.jsx';
-import Ribbons from "./components/Ribbons.jsx";
-
+import SplashCursor from './components/SplashCursor.jsx';
 
 const App = () => {
   return (
     <div className="container mx-auto max-w-7xl ">
       <div className="fixed inset-0 z-[1] ">
-        <Ribbons
+        <SplashCursor
           baseThickness={19}
           colors={["#5227FF", "#06a597"]}
         />
